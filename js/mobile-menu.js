0c3f85d2b355c272dbe716ffb218699866d94a2c
EstEtyX <programinout@gmail.com>
2026-01-30 04:51:05 +0200

    reworked the code in the mobile-menu.js to use the navigation menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,13 +1,27 @@
-const mobileMenu = document.querySelector(".mobile-menu");
-const menuBtnOpen = document.querySelector(".menu-btn-open");
-const menuBtnClose = document.querySelector(".menu-btn-close");
+// const mobileMenu = document.querySelector(".mobile-menu");
+// const menuBtnOpen = document.querySelector(".menu-btn-open");
+// const menuBtnClose = document.querySelector(".menu-btn-close");
 
-const toggleMenu = () => mobileMenu.classList.toggle("is-open");
+// const toggleMenu = () => mobileMenu.classList.toggle("is-open");
+
 // const disableScroll = () =>
 //   document.body.classList.toggle("is-scroll-disabled");
 
-menuBtnOpen.addEventListener("click", toggleMenu);
-menuBtnClose.addEventListener("click", toggleMenu);
+// menuBtnOpen.addEventListener("click", toggleMenu);
+// menuBtnClose.addEventListener("click", toggleMenu);
 
 // menuBtnOpen.addEventListener("click", disableScroll);
 // menuBtnClose.addEventListener("click", disableScroll);
+
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuBtnOpen = document.querySelector(".menu-btn-open");
+const closeButtons = document.querySelectorAll(".js-close-menu");
+
+const openMenu = () => mobileMenu.classList.add("is-open");
+const closeMenu = () => mobileMenu.classList.remove("is-open");
+
+menuBtnOpen.addEventListener("click", openMenu);
+
+closeButtons.forEach(button => {
+  button.addEventListener("click", closeMenu);
+});
