63817f9c66406360cddbd2ee54a24aa073b9c01a
EstEtyX <programinout@gmail.com>
2026-01-30 06:13:48 +0200

    corrected mobile-menu and mobile-menu.js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,6 +1,6 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuBtnOpen = document.querySelector(".menu-btn-open");
-const closeButtons = document.querySelectorAll(".js-close-menu");
+const closeButtons = document.querySelectorAll(".menu-btn-close");
 
 let scrollPosition = 0;
 
@@ -56,63 +56,3 @@ document.addEventListener(
   },
   { passive: false },
 );
-
-// const mobileMenu = document.querySelector(".mobile-menu");
-// const menuBtnOpen = document.querySelector(".menu-btn-open");
-// const closeButtons = document.querySelectorAll(".js-close-menu");
-
-// const openMenu = () => {
-//   mobileMenu.classList.add("is-open");
-//   document.body.classList.add("no-scroll");
-// };
-
-// const closeMenu = () => {
-//   mobileMenu.classList.remove("is-open");
-//   document.body.classList.remove("no-scroll");
-// };
-
-// menuBtnOpen.addEventListener("click", openMenu);
-
-// closeButtons.forEach(button => {
-//   button.addEventListener("click", closeMenu);
-// });
-
-// document.addEventListener("keydown", event => {
-//   if (event.key === "Escape" && mobileMenu.classList.contains("is-open")) {
-//     closeMenu();
-//   }
-// });
-
-// const mobileMenu = document.querySelector(".mobile-menu");
-// const menuBtnOpen = document.querySelector(".menu-btn-open");
-// const closeButtons = document.querySelectorAll(".js-close-menu");
-
-// const openMenu = () => mobileMenu.classList.add("is-open");
-// const closeMenu = () => mobileMenu.classList.remove("is-open");
-
-// menuBtnOpen.addEventListener("click", openMenu);
-
-// closeButtons.forEach(button => {
-//   button.addEventListener("click", closeMenu);
-// });
-
-// document.addEventListener("keydown", event => {
-//   if (event.key === "Escape" && mobileMenu.classList.contains("is-open")) {
-//     closeMenu();
-//   }
-// });
-
-// const mobileMenu = document.querySelector(".mobile-menu");
-// const menuBtnOpen = document.querySelector(".menu-btn-open");
-// const menuBtnClose = document.querySelector(".menu-btn-close");
-
-// const toggleMenu = () => mobileMenu.classList.toggle("is-open");
-
-// const disableScroll = () =>
-//   document.body.classList.toggle("is-scroll-disabled");
-
-// menuBtnOpen.addEventListener("click", toggleMenu);
-// menuBtnClose.addEventListener("click", toggleMenu);
-
-// menuBtnOpen.addEventListener("click", disableScroll);
-// menuBtnClose.addEventListener("click", disableScroll);
